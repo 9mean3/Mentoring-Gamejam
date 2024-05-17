2475f99a3c31db55d12ffc131201ef5f22f7c7e8
9mean3 <gminseong114@gmail.com>
2024-05-17 15:28:44 +0900

    룰타일 햇고 플랫피폼버드 햇다

--- Assets/01Scripts/Player/PlatformBird.cs
+++ Assets/01Scripts/Player/PlatformBird.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,10 +9,19 @@ public class PlatformBird : MonoBehaviour
     [SerializeField] private float _jumpPower;
 
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+
+    private BoxCollider2D _groundChecker;
+
+    private bool _isGround;
+    private int _curX;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _groundChecker = transform.Find("GroundChecker").GetComponent<BoxCollider2D>();
 
         _playerInput.MoveX += MoveXEvent;
         _playerInput.Jump += JumpEvent;
@@ -20,17 +29,36 @@ public class PlatformBird : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(_rigidbody.velocity);
+        //Debug.Log(_rigidbody.velocity);
+        if (_groundChecker.IsTouchingLayers(LayerMask.NameToLayer("Ground")))
+        {
+            _isGround = true;
+        }
+        else
+        {
+            _isGround = false;
+        }
     }
 
     private void MoveXEvent(float input)
     {
         float vel = input * _moveSpeed;
         _rigidbody.velocity = new Vector2(vel, _rigidbody.velocity.y);
+
+        if (Mathf.Abs(_rigidbody.velocity.x) > 0.1f)
+        {
+            SpriteFlipX(_rigidbody.velocity.x);
+        }
     }
 
     private void JumpEvent()
     {
-        _rigidbody.velocity += Vector2.up * _jumpPower;
+        if (_isGround)
+            _rigidbody.velocity += Vector2.up * _jumpPower;
+    }
+
+    private void SpriteFlipX(float dir)
+    {
+        _spriteRenderer.flipX = dir > 0 ? false : true;
     }
 }
